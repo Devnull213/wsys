79e48e6d16b0eb995b8617b1c9a55ad2e0189cb1
Devnull <guerra.defranchi@protonmail.com>
2022-04-10 03:07:46 -0400

    Sys replaced by argparse, and addition of small documentation

--- wsys.py
+++ wsys.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-import subprocess, re, sys
+import subprocess
+import re 
+import argparse
 
-
-def get_ttl(ip):
+def get_ttl(ip) -> int:
+    '''Takes an ip address and return the vulue'''
     proc = subprocess.run(['ping', '-c', '1', '-w', '2', ip], capture_output = True)
     search = re.search('ttl=\d{2,3}', proc.stdout.decode())
     return search.group()[4:]
 
 def detect_os(ttl, ip):
+    ''' Evaluation of OS by the ttl value'''
     if int(ttl) <= 64 and int(ttl) >= 0:
         return f'''[*] ttl = {ttl} -> OS Linux\n[*] There\'s {64 - int(ttl)} nodes between your machine and the objective -> {ip}.'''
 
@@ -19,15 +22,15 @@ def detect_os(ttl, ip):
         return 'The OS cannot be detected.'
 
 def main():
-    if len(sys.argv) != 2:
-        print('[!] Usage: wsys <IP_ADDRESS>, example -> wsys 8.8.8.8')
-    else:
-        ttl = get_ttl(sys.argv[1])
-        print(detect_os(ttl, sys.argv[1]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest= "ip_address", help="This argument require and should be an ip address or web domain.")
+    args = parser.parse_args()
+
+    ttl = get_ttl(args.ip_address)
+    print(detect_os(ttl, args.ip_address))
 
 
 if __name__ == '__main__':
 
     main()
-
 
