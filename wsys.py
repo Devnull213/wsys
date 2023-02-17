d2dbea0a05968b947869ba6fb7ac81fc7bf7c112
Devnull213 <michguerra13@hostv6.entelchile.net>
2023-02-16 22:42:33 -0300

    Added pep8 style and docstrings

--- wsys.py
+++ wsys.py
@@ -1,39 +1,67 @@
 #!/usr/bin/env python3
-
-import subprocess
-import re 
+""" Module that provides information about the OS by the ttl information"""
 import argparse
+import re
+import subprocess
 
-def get_ttl(ip) -> int:
-    '''Takes an ip or domain address and return the vulue'''
-    proc = subprocess.run(['ping', '-c', '1', '-w', '2', ip], capture_output = True)
+
+def get_ttl(ip_or_domain: str) -> tuple:
+    """
+    Returns the ttl value from the ping command aplied to a given ip address or domain
+
+        Parameters: 
+            ip_or_domain (string): Ip address or web domain
+
+        Returns:
+            integer ttl value
+    """
+    proc = subprocess.run(
+        ["ping", "-c", "1", "-w", "2", ip_or_domain], capture_output=True, check=True
+    )
     try:
-        search = re.search('ttl=[0-9]{2,3}', proc.stdout.decode())
-        return int(search.group()[4:])
-    except:
-        return -1
+        search = re.search("ttl=[0-9]{2,3}", proc.stdout.decode())
+        return (int(search.group()[4:]), ip_or_domain)
+    except ValueError as exc:
+        raise ValueError(exc) from exc
 
-def detect_os(ttl, ip):
-    ''' Evaluation of OS by the ttl value'''
-    if ttl <= 64 and ttl >= 0:
-        return f'''[*] ttl = {ttl} -> OS Linux\n[*] There\'s {64 - int(ttl)} nodes between your machine and the objective -> {ip}.'''
 
-    elif ttl <= 128 and ttl >= 64:
-        return f'''[*] ttl = {ttl} -> OS Windows\n[*] There\'s {128 - int(ttl)} nodes between your machine and the objective -> {ip}.'''
+def detect_os(ttl: int, ip_or_domain: str) -> str:
+    """
+    Evaluate the OS by the ttl value
 
-    else:
-        return 'The OS cannot be detected.'
+        Parameters:
+            ttl (int): ttl value from a ping command
+            ip_or_domain: Ip address or web domain
 
-def main():
+        Returns: 
+            String with the detected OS
+    """
+    if ttl <= 64:
+        return f"""
+        [*] ttl = {ttl} -> OS Linux
+        [*] There\'s {64 - ttl} nodes between your machine and the objective -> {ip_or_domain}."""
+
+    if 128 >= ttl >= 64:
+        return f"""
+        [*] ttl = {ttl} -> OS Windows
+        [*] There\'s {128 - ttl} nodes between your machine and the objective -> {ip_or_domain}."""
+
+    return "The OS cannot be detected."
+
+
+def main() -> None:
+    """Main execution function"""
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest= "ip_address", help="This argument require and should be an ip address or web domain.")
+    parser.add_argument(
+        dest="ip_address",
+        help="This argument require and should be an ip address or web domain.",
+    )
     args = parser.parse_args()
 
-    ttl = get_ttl(args.ip_address)
-    print(detect_os(ttl, args.ip_address))
+    ttl, ip_address = get_ttl(args.ip_address)
+    print(detect_os(ttl, ip_address))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     main()
-
